3b3e2e6a46432702f15cf628f83ef6fa2d7a74dc
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-11 03:09:49 +0530

    hashmap operations

--- src/main.rs
+++ src/main.rs
@@ -95,4 +95,72 @@ fn main() {
         println!("{}", b);
     }
 
+    // Hashmap
+
+    // Creating a New Hash Map
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let mut scores: HashMap<_, _> =
+        teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    // Hash Maps and Ownership
+    // For types that implement the Copy trait, like i32, the values are copied into the hash map. 
+    // For owned values like String, the values will be moved and the hash map will be the owner of those values,
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    // Accessing Values in a Hash Map
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    // Updating a Hash Map
+
+    // Overwriting a Value
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10); // add new key value
+    scores.insert(String::from("Blue"), 25); // override existing key's value
+
+    println!("{:?}", scores);
+    
+    // Only Inserting a Value If the Key Has No Value
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50); // inserts new key and value
+    scores.entry(String::from("Blue")).or_insert(50); // this key already exists so nothing happens
+
+    println!("{:?}", scores);
+
+    // Updating a Value Based on the Old Value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
